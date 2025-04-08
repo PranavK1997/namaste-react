5183b79c81ed76d7d350ba8301df572a6bbc7ef6
Arun Jadhav <arun@hihello.com>
2025-04-08 18:50:08 +0530

    Search box and useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,46 @@
 import RestaurantCard from "./RestaurantCard";
-import restroList from "./../utils/mockData"
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    // const [listOfRestaurants, setListOfRestaurants] = useState(restroList);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
-    const arr = useState(restroList);
-    const [listOfRestaurants, setListOfRestaurants] = arr;
-    return (
+    useEffect(() => {
+      fetchData()
+    }, [])
+
+    const fetchData = async () => {
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5642452&lng=73.7768511&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+      const json = await data.json();
+
+      console.log(json);
+      const restaurants = json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+      setListOfRestaurants(restaurants);
+      setFilteredRestaurants(restaurants);
+    }
+
+    return filteredRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+              <div className="search">
+                <input type="text" className="search-box" value={searchText} onChange={(e) =>{setSearchText(e.target.value)}}/>
+                <button onClick={() => {
+                  const filteredRestaurant = listOfRestaurants.filter((restaurant) =>
+                    restaurant.info.name.toLowerCase().includes(searchText)
+                  );
+                  setFilteredRestaurants(filteredRestaurant);
+                }}>Search</button>
+              </div>
                 <button className="filter-btn" onClick={() => {
-                   let filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
-                   setListOfRestaurants(filteredList);
+                   let filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4.5);
+                   setFilteredRestaurants(filteredList);
                 }}>Top Rated Restaurants</button>
             </div>
             <div className="restro-container">
   {
-    listOfRestaurants.map((restaurants) => {
+    filteredRestaurants.map((restaurants) => {
       return <RestaurantCard key={restaurants.info.id} restroData={restaurants} />;
     })
   }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,4 +2,4 @@ export const CDN_URL =
     "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/";
 
 export const LOGO_URL = 
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjZIYxLF_3jB3_SC9lTJPBTbmT_WAT42VXzg&s";
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdaMYtVi9_tfNcpsbGGseU6ehYgV9UeU3h7A&s";
